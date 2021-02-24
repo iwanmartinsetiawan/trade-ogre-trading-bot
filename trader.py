08be3c9e4968cc7692c36683f540eb812dbb1034
= <=>
2021-02-24 07:04:46 -0500

    make code decent

--- trader.py
+++ trader.py
@@ -1,82 +1,91 @@
-'''
-THIS IS A CRYPTOCURRENCY TRADING BOT
+import requests
+import json
+import time
+import datetime
+import sys
 
-USE AT YOUR OWN RISK, AS YOU HAVE A GOOD CHANCE OF LOSING MONEY (Although you can make $ too!)
+trade_ogre_api_key = 'xdlolmao'
+trade_ogre_secret_key = 'xdlolmao'
+COINS = ['RVN', 'LTC', 'XMR', 'ETH']
 
-BTC TIPS: 3QGG4dJbbNhmJSkoAUFDF5VvHZ7sJ7zZ5z
+debug = '--debug' in sys.argv
+base_url = 'https://tradeogre.com/api/v1'
 
-LTC TIPS: LNZGxrFtdaF6GKjnFBPegPbex9jrN1h4SQ
-
-RVN TIPS (KAAAAAW): RGT5QK9jwurbFZJCAqrF281cQSjU9LQhDV
-'''
-
-trade_ogre_api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
-
-trade_ogre_secret_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
-
-btc_fed = 0.01 #Put in amount of BTC in acct. Round up.
-
-risk = 0.00013
-
-# DO NOT EDIT CODE BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING!
-
-import requests, json, time
-
-base_url = 'https://tradeogre.com/api/v1' # Base URL to add /XXXX to, doesn't return anything
-min_profit_per_trade = btc_fed*0.0002
+def timestamp_print(msg):
+	date = datetime.datetime.now()
+	print(f'\u001b[36m[ {date.year}{date.month}{date.day} {date.hour}:{date.minute} ]\u001b[37m {msg}')
 
 class TradeOgre:
-
 	def __init__(self, trade_ogre_api, trade_ogre_secret):
 		self.trade_ogre_secret = trade_ogre_secret
 		self.trade_ogre_api = trade_ogre_api
 
-	def get_market_info(self):
+	def get_market_info(self, coin):
 		'''
-		LTC data at [30]['BTC-LTC']
+		RVN data at [30]['BTC-RVN']
 		'''
 
 		api_response = requests.get(base_url + '/markets', headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {self.trade_ogre_secret}'})
+		j = json.loads(api_response.content.decode('utf-8'))		
 
 		if api_response.status_code == 200:
-			return json.loads(api_response.content.decode('utf-8'))
+			for i in j:
+				if [k for k in i.keys()][0] == 'BTC-' + coin:
+					return i['BTC-' + coin]
 		else:
 			return None
 
-	def buy_ltc(self, amount, price):
+	def buy_coin(self, coin, amount, price):
 		'''
-		Buys LTC
+		Buys RVN
 
 		MIN AMOUNT IN BTC IS 0.0001
 		'''
-		api_response = requests.post(base_url + '/order/buy', data = {'market':'BTC-LTC', 'quantity':amount, 'price':price}, auth = (self.trade_ogre_api, self.trade_ogre_secret))
-		print(f'[!] Buy placed for {amount} LTC at {price} BTC, total of {round(amount*price, 6)} BTC')
-		print(f'[i] Server Code {api_response.status_code}')
+		sats = str(price * 100000000)
+		sats = sats.split('.')[0] if '.' in sats else sats
+
+		total = format(amount*price, '.8f')
+
+		api_response = requests.post(base_url + '/order/buy', {'market': 'BTC-' + coin, 'quantity': str(round(amount, 2)), 'price': format(price, '.8f')}, auth = (self.trade_ogre_api, self.trade_ogre_secret))
+		
+		if json.loads(api_response.text)['success']:
+			timestamp_print(f'[!] Buy placed for {amount} {coin} at {sats} satoshis, total of {total} BTC')
+		
+		if debug:
+			timestamp_print(json.loads(api_response.content.decode('utf-8')))
 		return json.loads(api_response.content.decode('utf-8'))
 
 
-	def sell_ltc(self, amount, price):
+	def sell_coin(self, coin, amount, price):
 		'''
-		Sells LTC
+		Sells RVN
 
 		MIN AMOUNT IN BTC IS 0.0001
 		'''
-		api_response = requests.post(base_url + '/order/sell', data = {'market':'BTC-LTC', 'quantity':amount, 'price':price}, auth = (self.trade_ogre_api, self.trade_ogre_secret))
-		print(f'[!] Sell placed for {amount} LTC at {price} BTC, total of {round(amount*price, 6)} BTC')
-		print(f'[i] Server Code {api_response.status_code}')
+		sats = str(price * 100000000)
+		sats = sats.split('.')[0] if '.' in sats else sats
+
+		total = format(amount*price, '.8f')
+
+		api_response = requests.post(base_url + '/order/sell', {'market':'BTC-' + coin, 'quantity':amount, 'price': format(price, '.8f')}, auth = (self.trade_ogre_api, self.trade_ogre_secret))
+		
+		if json.loads(api_response.text)['success']:
+			timestamp_print(f'[!] Sell placed for {amount} {coin} at {sats} satoshis, total of {total} BTC')
+		
+		if debug:
+			timestamp_print(json.loads(api_response.content.decode('utf-8')))
 		return json.loads(api_response.content.decode('utf-8'))
 
 
 	def cancel_order(self, uuid):
 		api_response = requests.post(base_url + '/order/cancel', data = {'uuid':uuid}, auth = (self.trade_ogre_api, self.trade_ogre_secret))
-		print(f'[!] Order cancelled for {uuid}')
-		print(f'[i] Server Code {api_response.status_code}')
+		timestamp_print(f'[!] Order cancelled for {uuid}')
 		return json.loads(api_response.content.decode('utf-8'))
 
 
 	def get_bal(self, currency_ticker):
 		api_response = requests.get(base_url + '/account/balances', auth = (self.trade_ogre_api, self.trade_ogre_secret))
-		return json.loads(api_response.content.decode('utf-8'))['balances'][currency_ticker]
+		return float(json.loads(api_response.content.decode('utf-8'))['balances'][currency_ticker])
 
 	def get_order(self):
 		api_response = requests.post(base_url + '/account/orders', auth = (self.trade_ogre_api, self.trade_ogre_secret))
@@ -95,21 +104,24 @@ class TradeOgre:
 		return prev_orders
 
 
-
-
-
 class Binance:
 
-	def get_market_info(self):
+	def get_market_info(self, coin):
 		'''
-		LTC data at [1]
+		RVN data at [398]
 		'''
 		api_response = requests.get('https://api.binance.com/api/v1/ticker/24hr')
-		return json.loads(api_response.content.decode('utf-8'))
 
-	def get_order_book(self):
-		#/api/v1/depth
-		api_response = requests.get('https://api.binance.com/api/v1/depth', params = {'symbol': 'LTCBTC', 'limit':100})
+		for c in json.loads(api_response.content.decode('utf-8')):
+			if c['symbol'] == coin + 'BTC':
+				if debug:
+					timestamp_print(c)
+				return c
+		if debug:
+			timestamp_print(f'Binance.get_market_info("{coin}") returned None!')
+
+	def get_order_book(self, ticker):
+		api_response = requests.get('https://api.binance.com/api/v1/depth', params = {'symbol': ticker, 'limit':100})
 		return json.loads(api_response.content.decode('utf-8'))
 
 
@@ -122,257 +134,48 @@ Create classes with API keys.
 binance = Binance()
 trade_ogre = TradeOgre(trade_ogre_api_key, trade_ogre_secret_key)
 
+def get_day_low(coin):
+	binance_day_low = float(binance.get_market_info(coin)['lowPrice'])
+	trade_ogre_day_low = float(trade_ogre.get_market_info(coin)['low'])
 
-def show_bal():
-	x = trade_ogre.get_bal('BTC')
-	y = trade_ogre.get_bal('LTC')
+	return (binance_day_low + trade_ogre_day_low) / 2
 
-	print(f'[^] Your BTC balance is {x}, the starting amount was {base_btc_bal}.')
-	print(f'[^] Your LTC balance is {y}, the starting amount was {base_bal_ltc}.')
+def get_day_high(coin):
+	binance_day_hi = float(binance.get_market_info(coin)['highPrice'])
+	trade_ogre_day_hi = float(trade_ogre.get_market_info(coin)['high'])
 
-
-def check_profit(buy_price, sell_price):
-	print(f'[i] Possible Profit: {sell_price - buy_price}')
-
-	if (sell_price - buy_price) <= min_profit_per_trade:
-		return False
-	else:
-		return True
+	return (binance_day_hi + trade_ogre_day_hi) / 2
 
 
+def buy_low(coin):
+	price = get_day_low(coin) + 0.00000001
+	return trade_ogre.buy_coin(coin, (trade_ogre.get_bal('BTC') / len(COINS)) / price, price)['success']
 
-# ALGO ONE STARTS HERE!___________________________________________________________________________________________________________________
-
-bulk_trade_data = [['0', 'INDEXFILLER' , 1], ['1', 'INDEXFILLER', 0]]
-trade_counter = 0
-
-prev_buy = float(trade_ogre.get_order()[0])
-prev_sell = float(trade_ogre.get_order()[1])
-
-def update_ave():
-	binance_day_hi = float(binance.get_market_info()[1]['highPrice'])
-	trade_ogre_day_hi = float(trade_ogre.get_market_info()[30]['BTC-LTC']['high'])
-
-	ave_hi = (binance_day_hi+trade_ogre_day_hi)/2
-
-	binance_day_low = float(binance.get_market_info()[1]['lowPrice'])
-	trade_ogre_day_low = float(trade_ogre.get_market_info()[30]['BTC-LTC']['low'])
-
-	ave_low = (binance_day_low+trade_ogre_day_low)/2
-	return (ave_low, ave_hi)
-
-
-def evan_slave_buy_low(total=1):
-	'''
-	Total needs to be at least 1, it works like total * min purchace amount
-	'''
-	global trade_counter, bulk_trade_data, prev_buy
-
-	max_buy_amount = float(trade_ogre.get_bal('BTC')) / float(binance.get_market_info()[1]['highPrice'])
-
-	day_low = update_ave()[0]
-
-	#FIND MIN AMOUNT NEEDED TO BUY AT MIN PRICE
-	day_low_risk = day_low + risk
-
-	bulk_trade_data.append([str(trade_counter), 'INDEXFILLER', max_buy_amount])
-	prev_buy = day_low_risk
-
-	if check_profit(prev_buy, prev_sell) == True:
-		trade_data = trade_ogre.buy_ltc(round(max_buy_amount, 8), round(day_low_risk, 8))
-
-		trade_counter += 1
-	else:
-		print('Unprofitable Trade Averted Captian!')
-
-
-def evan_slave_sell_high(total=1):
-	'''
-	Total needs to be at least 1, it works like total * min purchace amount
-	'''
-	global trade_counter, bulk_trade_data, prev_sell
-
-	max_sell_amount = float(trade_ogre.get_bal('LTC')) - 0.00001
-
-	day_hi = update_ave()[1]
-
-
-	day_hi_risk = day_hi - risk
-	bulk_trade_data.append([str(trade_counter), 'INDEXFILLER', max_sell_amount])
-	prev_sell = day_hi_risk
-
-	if check_profit(prev_buy, prev_sell) == True:
-		trade_data = trade_ogre.sell_ltc(round(max_sell_amount, 8), round(day_hi_risk, 8))
-
-		trade_counter += 1
-	else:
-		print('Unprofitable Trade Averted Captian!')
-
-
-
-num = 0
-def evan_slave_cancel_algo_one(uuid):
-	global bulk_trade_data, trade_counter, num
-
-	trade_ogre.cancel_order(uuid)
-
-	if num != 0:
-		bulk_trade_data = []
-		trade_counter = 0
-	else:
-		num = 1
+def sell_high(coin):
+	return trade_ogre.sell_coin(coin, trade_ogre.get_bal(coin) / 2, get_day_high(coin) - 0.00000001)['success']
 
 def algo_one():
-	base_btc_bal = trade_ogre.get_bal('BTC')
-	base_bal_ltc = trade_ogre.get_bal('LTC')
-
-	print(f'[^] Your BTC balance is {base_btc_bal}.')
-	print(f'[^] Your LTC balance is {base_bal_ltc}.')
-
-	minutes = 0
-
+	print('\u001b[37m', end='')
 
 	while True:
-		print('\n')
-		evan_slave_cancel_algo_one('all')
-		print('\n')
-
-		evan_slave_buy_low()
-		print('\n')
-
-		evan_slave_sell_high()
-		print('\n')
-
-		print(f"[^] Your BTC balance is {trade_ogre.get_bal('BTC')}, change of {float(trade_ogre.get_bal('BTC'))-float(base_btc_bal)} (If no '-' it is positive)")
-		print(f"[^] Your LTC balance is {trade_ogre.get_bal('LTC')}, change of {float(trade_ogre.get_bal('LTC'))-float(base_bal_ltc)} (If no '-' it is positive)")
-		print(f"[:] It's been {minutes} minute(s).")
-		print('_' * 208)
-
-
-		time.sleep(60)
-		minutes += 1
-
-# ALGO ONE END HERE!___________________________________________________________________________________________________________________
-
-'''
-BINANCE DEPTH DATA
-
-{'lastUpdateId': 166970061, 'bids': [['0.01171200', '0.17000000', []], ['0.01171100', '6.09000000', []], ['0.01170800', '30.47000000', []],
- ['0.01170700', '38.12000000', []], ['0.01170400', '74.47000000', []]], 
-
-'asks': [['0.01171700', '0.34000000', []], ['0.01172100', '27.04000000', []], ['0.01172200', '68.59000000', []], ['0.01172400', '80.93000000', []], 
-['0.01172600', '2.86000000', []]]}
-'''
-
-# ALGO TWO START HERE!.................................................................................................................
-
-
-prev_buy_2 = prev_buy
-prev_sell_2 = prev_sell
-
-def evan_slave_sell_with_depth():
-	global prev_sell_2
-
-	chart = binance.get_order_book()
-	ideal_sell = 0
-	for c_data in chart['bids']:
-		bid_price = float(c_data[0])
-		bid_amount = float(c_data[1])
-
-		bid_total = bid_price * bid_amount
-		ideal_sell += bid_total
-
-		if ideal_sell < 1000:
-			if check_profit(prev_buy_2, prev_sell_2) == True:
-				max_sell_amount = float(trade_ogre.get_bal('LTC')) - 0.00001
-				trade_ogre.sell_ltc(round(max_sell_amount, 8), round(bid_price, 8))
-				prev_sell_2 = bid_price
-				print(bid_price)
-				print(prev_sell_2)
-				break
-
-			else:
-				print(bid_price)
-				print(prev_sell_2)
-				print('Unprofitable Trade Averted Captian!')
-				break
-
-
-def evan_slave_buy_with_depth():
-	global prev_buy_2
-
-	chart = binance.get_order_book()
-	ideal_buy = 0
-	for c_data in chart['asks']:
-		ask_price = float(c_data[0])
-		ask_amount = float(c_data[1])
-
-		ask_total = ask_price * ask_amount
-		ideal_buy += ask_total
-
-		if ideal_buy < 1000:
-			if check_profit(prev_buy_2, prev_sell_2) == True:
-
-				max_buy_amount = float(trade_ogre.get_bal('BTC')) * 80
-				trade_ogre.buy_ltc(round(max_buy_amount, 8), round(ask_price, 8))
-				prev_buy_2 = ask_price
-				print(ask_price)
-				print(prev_buy_2)
-				break
-
-			else:
-				print(ask_price)
-				print(prev_buy_2)
-				print('Unprofitable Trade Averted Captian!')
-				break
-
-
-def algo_two():
-	base_btc_bal = trade_ogre.get_bal('BTC')
-	base_bal_ltc = trade_ogre.get_bal('LTC')
-
-	print(f'[^] Your BTC balance is {base_btc_bal}.')
-	print(f'[^] Your LTC balance is {base_bal_ltc}.')
-
-	minutes = 0
-
-	while True:
-		print('\n')
 		trade_ogre.cancel_order('all')
-		print('\n')
 
-		evan_slave_sell_with_depth()
-		print('\n')
+		for c in COINS:
+			buy_low(c)
+			sell_high(c)
 
-		evan_slave_buy_with_depth()
-		print('\n')
+		timestamp_print(f"\u001b[33m[^] Your BTC balance is {trade_ogre.get_bal('BTC')}")
 
-		print(f"[^] Your BTC balance is {trade_ogre.get_bal('BTC')}, change of {float(trade_ogre.get_bal('BTC'))-float(base_btc_bal)} (If no '-' it is positive)")
-		print(f"[^] Your LTC balance is {trade_ogre.get_bal('LTC')}, change of {float(trade_ogre.get_bal('LTC'))-float(base_bal_ltc)} (If no '-' it is positive)")
-		print(f"[:] It's been {minutes} minute(s).")
-		print('_' * 208)
+		for c in COINS:
+			timestamp_print(f"\u001b[33m[^] Your {c} balance is {trade_ogre.get_bal(c)}")
+		
+		print('\u001b[37m' + '-' * 30)
 
+		time.sleep(600)
 
-		time.sleep(20)
-		minutes += 50
-
-
+if __name__ == '__main__':	
+	algo_one()
 
 
-
-if __name__ == '__main__':
-	'''
-	Litle user interface, no need until more algorithems are developed. Mostly cuz algo two SUKZ
-	while True:
-		choose_algo = input('Which algorithm do you want to use? Algo 1 is most popular. [1/2] ')
-		if choose_algo == '1':
-			algo_one()
-		elif choose_algo == '2':
-			algo_two()
-		else:
-			print('Only use 1 or 2.')
-	'''
-	
-	algo_one() # DO NOT USE ALGO 2
-
-
+# jesus christ this was the 1st real project i made and i just came back to it 2 years later... VERY glad i dont code like this anymore
+# cuz holy crap this code is dogshit
